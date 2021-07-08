c36b09f74da227d1cc6072a3b4139f416c212c67
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2021-07-08 13:02:08 +0530

    Reorg files

--- cmake/llvm_arm_toolchain.cmake
+++ cmake/llvm_arm_toolchain.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
 # Clang target triple
-SET(TARGET armv7hf-linux-gnueabihf)
+SET(TARGET arm-linux-gnueabihf)
 
 # specify the cross compiler
 SET(CMAKE_C_COMPILER_TARGET ${TARGET})
@@ -12,8 +12,9 @@ SET(CMAKE_CXX_COMPILER clang++-11)
 SET(CMAKE_ASM_COMPILER_TARGET ${TARGET})
 SET(CMAKE_ASM_COMPILER clang-11)
 
-set(CMAKE_SYSROOT "/home/ruvinda/x-tools/arm-cortex_a8-linux-gnueabihf/arm-cortex_a8-linux-gnueabihf/sysroot")
-SET(ARCH_FLAGS "-target armv7hf-linux-gnueabihf -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=hard -fuse-ld=lld-11")
+#set(CMAKE_SYSROOT "/home/ruvinda/x-tools/arm-cortex_a8-linux-gnueabihf/arm-cortex_a8-linux-gnueabihf/sysroot")
+set(CMAKE_SYSROOT /)
+SET(ARCH_FLAGS "-target arm-linux-gnueabihf")
 SET(CMAKE_C_FLAGS ${ARCH_FLAGS})
 SET(CMAKE_CXX_FLAGS ${ARCH_FLAGS})
 
